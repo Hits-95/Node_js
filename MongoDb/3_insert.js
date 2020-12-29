cf70705fda30c1bc8422472ab0205a7fba601278
Ahire Hitesh <hbahire2014@gmail.com>
2020-12-29 19:21:01 +0530

    update

--- MongoDb/3_insert.js
+++ MongoDb/3_insert.js
@@ -1,11 +1,11 @@
 const MongoClient = require("mongodb").MongoClient;
 
-var url = "mongodb+srv://Hitesh:online@cluster0.w9ui5.mongodb.net/<dbname>?retryWrites=true&w=majority";
+var url = "mongodb+srv://Hitesh:online@cluster0.w9ui5.mongodb.net/node?retryWrites=true&w=majority";
 MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     console.log("connected...");
     var database = db.db("node");
-    var obj = { _id: 1, name: "hitesh", addr: "dabli" };
+    var obj = { _id: 13, name: "Ram", addr: "dabli" };
     database.collection("customers").insertOne(obj, function (err, res) {
         if (err) throw err;
         console.log("1 : ducument inserted...");
